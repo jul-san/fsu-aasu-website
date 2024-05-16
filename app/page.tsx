68bd2e25d11614cbf7bf10b83d02cde8d29c7b57
Julian Sanchez <julianwsan@gmail.com>
2024-05-16 03:34:53 -0400

    added footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import Navbar from "../app/components/Navbar";
 import Banner from "../app/components/Banner";
+import AboutBanner from "../app/components/AboutBanner";
+import OurHistory from "../app/components/OurHistory";
+import Footer from "../app/components/Footer";
+import { Box } from "@chakra-ui/react";
 
 
 export default function Home() {
@@ -8,6 +12,9 @@ export default function Home() {
     <>
       <Navbar />
       <Banner />
+      <AboutBanner />
+      <OurHistory />
+      <Footer />
     </>
   );
 }
